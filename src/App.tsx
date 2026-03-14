fa094a383c012d5375bdc7c138a98ed1fd93eb48
Skye Brady <skye@blueskye.co.uk>
2026-03-14 22:37:39 +0000

    fix: use custom icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import './App.css'
 import { useAisData } from './useAisData';
+import { Icon } from 'leaflet';
 
 function App() {
   const positions = useAisData();
@@ -12,14 +13,12 @@ function App() {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       {Object.values(positions).map((pos) => (
-        <Marker key={pos.mmsi} position={[pos.latitude, pos.longitude]}>
+        <Marker key={pos.mmsi} position={[pos.latitude, pos.longitude]} icon={new Icon({ iconUrl: 'boat.png', attribution: '&copy; <a href="https://www.flaticon.com/">FlatIcon.com</a>', iconSize: [25, 25] })}>
           <Popup>
             MMSI: {pos.mmsi}<br />
             Heading: {pos.trueHeading}°<br />
             Speed: {pos.speedOverGround} knots<br />
             Timestamp: {new Date(pos.msgtime).toLocaleString()}<br />
-            {pos.latitude}<br />
-            {pos.longitude}
           </Popup>
         </Marker>
       ))}
