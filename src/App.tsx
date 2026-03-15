5e76e9f6ab0d93b6c90c40774e313b3742c6a0ca
Skye Brady <skye@blueskye.co.uk>
2026-03-15 18:05:18 +0000

    split map component out

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,12 @@
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
-import './App.css'
-import { useAisData } from './useAisData';
-import { Icon } from 'leaflet';
+import "./App.css";
+import { Map } from "./Map";
 
 function App() {
-  const positions = useAisData();
   return (
     <div className="App">
-    <MapContainer center={[65.76, 7.52]} zoom={5} className='map'>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {Object.values(positions).map((pos) => pos.latitude && pos.longitude && (
-        <Marker key={pos.mmsi} position={[pos.latitude, pos.longitude]} icon={new Icon({ iconUrl: 'boat.png', attribution: '&copy; <a href="https://www.flaticon.com/">FlatIcon.com</a>', iconSize: [25, 25] })}>
-          <Popup>
-            {pos.vesselName && <>
-            {pos.vesselName}{pos.callsign && ` (${pos.callsign})`}<br />
-            {pos.destination && <>Destination: {pos.destination}<br /></>}</>}
-            MMSI: {pos.mmsi}<br />
-            {pos.trueHeading !== undefined && <>Heading: {pos.trueHeading}°<br /></>}
-            {pos.speedOverGround !== undefined && <>Speed: {pos.speedOverGround} knots<br /></>}
-            {pos.msgtime && <>Timestamp: {new Date(pos.msgtime).toLocaleString()}<br /></>}
-          </Popup>
-        </Marker>
-      ))}
-    </MapContainer>
+      <Map />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
